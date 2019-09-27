183c157053411a5ba429931dbd3f1915ab352140
allenlicolumbia@gmail.com <allenlicolumbia@gmail.com>
2019-09-27 01:25:21 -0400

    adding client and api resources for authorization

---
+++ IdentityServerIDP/Config.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServerIDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+    }
+}
